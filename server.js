a8b60a40ce8c6a91a72d3dbd82d7fd3a810858c4
glebas666 <glebknyazev43@gmail.com>
2024-12-02 23:35:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors'); // Для разрешения CORS
 const path = require('path'); // Для работы с путями
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Разрешаем CORS
 app.use(cors());
